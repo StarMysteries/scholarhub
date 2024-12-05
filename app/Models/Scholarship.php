1b4e8263dcc51dca75a156505906e4c93dcf1df9
Kyn Honoridez <141384964+kyn-creator@users.noreply.github.com>
2024-12-05 17:34:40 +0800

    Updated Scholarship.php

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -8,6 +8,15 @@ class Scholarship extends Model
 {
     protected $primaryKey = 'scholarship_id';
 
+    // Specify the fillable attributes for mass assignment
+    protected $fillable = [
+        'provider_id',
+        'scholarship_name',
+        'scholarship_desc',
+        'scholarship_status',
+        'scholarship_deadline'
+    ];
+
     public function applications()
     {
         return $this->hasMany(Application::class, 'scholarship_id', 'scholarship_id');
@@ -18,4 +27,3 @@ class Scholarship extends Model
         return $this->belongsToMany(Course::class, 'scholarship_courses', 'scholarship_id', 'course_id');
     }
 }
-
