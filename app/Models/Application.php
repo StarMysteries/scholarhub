e580f5b8e416d2a95179d875b837977cb4474244
Kyn Honoridez <141384964+kyn-creator@users.noreply.github.com>
2024-12-10 10:26:17 +0800

    Update Application.php

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,11 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    protected $primaryKey = 'application_id';
+    protected $primaryKey = 'application_id';   
+
+    protected $fillable = [
+        'student_id',
+        'scholarship_id',
+        'application_status',
+        'rejection_details',
+        'application_date',
+    ];
+
 
     // Relationship with the Scholarship table
     public function scholarship()
     {
         return $this->belongsTo(Scholarship::class, 'scholarship_id', 'scholarship_id');
     }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id', 'student_id');
+    }
+
 }
